3106b6093af5658987e14713c2eb8913442c5f40
Mechanic <52928434+Korkoe27@users.noreply.github.com>
2022-11-21 17:18:55 +0000

    Day 1/100 of learning php. Created a simple page to display an input from a user.

---
+++ validate.php
@@ -0,0 +1,28 @@
+<!--     <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>
+ -->
+
+    //<?php
+    // $email = "";
+    // $name = "";
+
+
+
+    // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //     // code...
+    //     $email = form_validate($_POST['$email']);
+    //     $name = form_validate($_POST['$name']);
+    // }
+ 
+       //  echo "Hello, ".$_POST['name'];
+
+     // ?>
