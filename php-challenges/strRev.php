39000687080fe92ddb755e04506e3d3173969558
Mechanic <52928434+Korkoe27@users.noreply.github.com>
2023-03-20 20:06:36 +0000

    Day #9/100. Started coding challenges to improve my algorithmic thinking. Wrote a program to reverse a string using a for loop.

---
+++ php-challenges/strRev.php
@@ -0,0 +1,20 @@
+<?php
+//Mon, March 20th ,2023.
+//This program takes a word from the user and flips it.
+//for example, loop becomes pool. and cool becomes looc.
+
+function stringReverse(string $userStr){
+
+    //first the string has been assigned to a variable called userStr;
+    //then the length of the string is put into a variable.
+   $stringLength = strlen($userStr);
+    
+
+   //in this for loop, i is instatiated at the end of the string. And until the position i is equal to zero, the position of i will keep decreasing by 1.
+    for($i = ($stringLength-1); $i >= 0; $i--){
+        echo $userStr[$i];
+    }
+    echo "<br>";
+}
+stringReverse("hello there");
+
