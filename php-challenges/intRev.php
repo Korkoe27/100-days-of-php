30aa2c24783d5a5f858ce7ae99e921fec9997c2d
Mechanic <52928434+Korkoe27@users.noreply.github.com>
2023-03-24 20:53:13 +0000

    Started working on a string reversal program.

---
+++ php-challenges/intRev.php
@@ -0,0 +1,10 @@
+<?php
+//this program reverses an integer and outputs the result.
+//eg 234 returns 432. and -472 returns -274
+
+
+function integerRev(int $integer){
+    if($integer>0){
+        
+    }
+}
